d962de67d73cf757f8e17edc8a828f6c742b04d8
devanshi ganatra <devanshiganatra93@gmail.com>
2019-02-14 22:20:56 -0500

    implemented TopicService

--- src/main/java/com/example/wbsp19devanshiganatraserverside/services/TopicService.java
+++ src/main/java/com/example/wbsp19devanshiganatraserverside/services/TopicService.java
@@ -2,7 +2,7 @@ package com.example.wbsp19devanshiganatraserverside.services;
 
 import com.example.wbsp19devanshiganatraserverside.model.Topic;
 import com.example.wbsp19devanshiganatraserverside.model.Widget;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +16,59 @@ public class TopicService {
         widgets.add(widget1);
     }};
 
+    List<Topic> topics = new ArrayList<Topic>(){{
+        topics.add(topic1);
+    }};
+
     Topic topic1= new Topic(1, "Topic1", widgets);
+
+    @PostMapping("/api/lesson/{lid}/topic")
+    public Topic createTopic(
+            @PathVariable ("lid") Integer lid  , @RequestBody Topic topic) {
+        topics.add(topic);
+        return topic;
+    }
+
+
+    @GetMapping("/api/lesson/{lid}/topic/")
+    public List<Topic> findAllTopics(
+            @PathVariable("lid") Integer lid) {
+
+        return topics;
+    }
+
+    @GetMapping("/api/topic/{tid}")
+    public Topic findTopicById(@PathVariable("tid") Integer id) {
+        for (Topic topic : topics) {
+            if (id == topic.getId().intValue())
+                return topic;
+        }
+        return null;
+    }
+
+    @PutMapping("/api/topic/{tid}")
+    public void updateTopic(@RequestBody Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            if (topic.getId() == topics.get(i).getId().intValue()){
+
+                topics.get(i).setId(topic.getId());
+                topics.get(i).setTitle(topic.getTitle());
+                topics.get(i).setWidgets(topic.getWidgets());
+                break;
+            }
+        }
+
+    }
+
+    @DeleteMapping("/api/topic/{tid}")
+    public void deleteTopic(@PathVariable("tid") Integer id) {
+        for (int i = 0; i < topics.size(); i++) {
+            if (id == topics.get(i).getId().intValue()) {
+                topics.remove(i);
+            }
+        }
+
+    }
 
 
 
