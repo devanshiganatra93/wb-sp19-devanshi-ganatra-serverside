01762bf4985bcb62345133777e5078712f3aa164
devanshi ganatra <devanshiganatra93@gmail.com>
2019-02-14 22:56:10 -0500

    Lesson Service implemented

--- src/main/java/com/example/wbsp19devanshiganatraserverside/services/LessonService.java
+++ src/main/java/com/example/wbsp19devanshiganatraserverside/services/LessonService.java
@@ -3,7 +3,7 @@ package com.example.wbsp19devanshiganatraserverside.services;
 import com.example.wbsp19devanshiganatraserverside.model.Lesson;
 import com.example.wbsp19devanshiganatraserverside.model.Topic;
 import com.example.wbsp19devanshiganatraserverside.model.Widget;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +26,57 @@ public class LessonService {
 
     Lesson lesson1= new Lesson(1, "Lesson1", topics);
 
+    List<Lesson> lessons = new ArrayList<Lesson>(){{
+        lessons.add(lesson1);
+    }};
+
+
+    @PostMapping("/api/module/{mid}/lesson")
+    public Lesson createLesson(
+            @PathVariable("mid") Integer mid  , @RequestBody Lesson lesson) {
+        lessons.add(lesson);
+        return lesson;
+    }
+
+
+    @GetMapping("/api/module/{mid}/lesson")
+    public List<Lesson> findAllLessons(
+            @PathVariable("mid") Integer mid) {
+
+        return lessons;
+    }
+
+    @GetMapping("/api/lesson/{lid}")
+    public Lesson findLessonById(@PathVariable("lid") Integer id) {
+        for (Lesson lesson : lessons) {
+            if (id == lesson.getId().intValue())
+                return lesson;
+        }
+        return null;
+    }
+
+    @PutMapping("/api/lesson/{lid}")
+    public void updateLesson(@RequestBody Lesson lesson) {
+        for (int i = 0; i < lessons.size(); i++) {
+            if (lesson.getId() == lessons.get(i).getId().intValue()){
+                lessons.get(i).setId(lesson.getId());
+                lessons.get(i).setTitle(lesson.getTitle());
+                lessons.get(i).setTopics(lesson.getTopics());
+                break;
+            }
+        }
+
+    }
+
+    @DeleteMapping("/api/lesson/{lid}")
+    public void deleteLesson(@PathVariable("lid") Integer id) {
+        for (int i = 0; i < lessons.size(); i++) {
+            if (id == lessons.get(i).getId().intValue()) {
+                lessons.remove(i);
+            }
+        }
+
+    }
 
 
 }
