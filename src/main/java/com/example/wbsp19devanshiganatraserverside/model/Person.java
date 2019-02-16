057000ab7a9aa51c9dbd7ddb135da92bf3b75efb
devanshi ganatra <devanshiganatra93@gmail.com>
2019-02-16 04:45:48 -0500

    implemented Person and faculty Services

--- src/main/java/com/example/wbsp19devanshiganatraserverside/model/Person.java
+++ src/main/java/com/example/wbsp19devanshiganatraserverside/model/Person.java
@@ -1,4 +1,78 @@
 package com.example.wbsp19devanshiganatraserverside.model;
 
 public class Person {
+
+    private Long Id;
+    private String userName;
+    private String password;
+    private String email;
+    private Long phoneNumber;
+    private String firstName;
+    private String lastName;
+
+    public Person(Long id, String userName, String password, String email, Long phoneNumber, String firstName, String lastName) {
+        Id = id;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
---
+++ src/main/java/com/example/wbsp19devanshiganatraserverside/services/FacultyService.java
@@ -0,0 +1,72 @@
+package com.example.wbsp19devanshiganatraserverside.services;
+
+
+import com.example.wbsp19devanshiganatraserverside.model.Person;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class FacultyService {
+
+    Person person1 = new Person (111111L, "d", "g", "jhff", 9909503664L, "dev", "gan");
+
+    List<Person> persons = new ArrayList<Person>() {{
+        add(person1);
+    }};
+
+    @PostMapping("/api/register")
+    public Person register(@RequestBody Person person,
+                         HttpSession session) {
+        session.setAttribute("currentUser", person);
+        persons.add(person);
+        return person;
+    }
+
+
+    @PostMapping("/api/profile")
+    public Person profile(HttpSession session) {
+        Person currentUser = (Person)
+                session.getAttribute("currentUser");
+        return currentUser;
+    }
+
+    @PostMapping("/api/login")
+    public Person login(@RequestBody Person credentials,
+                          HttpSession session) {
+        for (Person person : persons) {
+            if( person.getFirstName().equals(credentials.getFirstName())
+                    && person.getPassword().equals(credentials.getPassword())) {
+                session.setAttribute("currentUser", person);
+                return person;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/api/logout")
+    public void logout
+            (HttpSession session) {
+        session.invalidate();
+    }
+
+    @GetMapping("/api/persons")
+    public List<Person> findAllPersons() {
+
+        return persons;
+    }
+
+    @GetMapping("/api/persons/{pid}")
+    public Person findPersonsById(@PathVariable("pid") Long id) {
+        for (Person person : persons) {
+            if (id == person.getId().longValue())
+                return person;
+        }
+        return null;
+    }
+
+
+}
